02ca11f19763735ae6fc6f6017b580ce8235a2a7
burcubiterr <70144117+burcubiterr@users.noreply.github.com>
2021-04-25 02:06:59 +0300

    Design Updated

--- MLRS2_CodePart/MLRS2/app/src/main/java/com/example/mlrs2/MainActivity.kt
+++ MLRS2_CodePart/MLRS2/app/src/main/java/com/example/mlrs2/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signedin)
+        setContentView(R.layout.activity_menu_lm)
     }
 }
