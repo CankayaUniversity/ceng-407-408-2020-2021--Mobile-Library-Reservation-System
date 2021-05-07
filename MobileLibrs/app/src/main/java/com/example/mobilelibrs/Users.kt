513236e53a9f1365433b7607a727b4a20740ada2
DESKTOP-8R5D2NM\BEYZA <yaratici2007@gmail.com>
2021-05-07 14:24:34 +0300

    Update

---
+++ MobileLibrs/app/src/main/java/com/example/mobilelibrs/Users.kt
@@ -0,0 +1,16 @@
+package com.example.mobilelibrs
+
+class Users {
+
+    var userid:Int = 1
+    var userName:String = ""
+    var userEmail:String = ""
+
+    constructor( userid:Int, userName:String, userEmail:String )
+    {
+        this.userid = userid
+        this.userName = userName
+        this.userEmail = userEmail
+    }
+
+}
