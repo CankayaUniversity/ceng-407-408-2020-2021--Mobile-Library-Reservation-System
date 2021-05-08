4af25948b3452ad01c9a2aac0dd6f75de9fe25cf
DESKTOP-8R5D2NM\BEYZA <yaratici2007@gmail.com>
2021-05-08 17:57:55 +0300

    Update Activity_LM_List_Libraries.kt

--- MobileLibrs/app/src/main/java/com/example/mobilelibrs/Activity_LM_List_Libraries.kt
+++ MobileLibrs/app/src/main/java/com/example/mobilelibrs/Activity_LM_List_Libraries.kt
@@ -7,7 +7,6 @@ import android.widget.ArrayAdapter
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.example.mobilelibrs.databinding.ActivityAdminLiblistBinding
 import com.example.mobilelibrs.databinding.ActivityLmListlibrariesBinding
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
@@ -15,7 +14,6 @@ import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import java.lang.StringBuilder
 import com.example.mobilelibrs.Library
-import com.example.mobilelibrs.databinding.ActivityAdminAddlibraryBinding
 
 class Activity_LM_List_Libraries : AppCompatActivity() {
 
