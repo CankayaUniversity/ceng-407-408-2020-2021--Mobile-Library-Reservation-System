7404238abdb323dfaf75f0c299d6281ef3d60a82
DESKTOP-8R5D2NM\BEYZA <yaratici2007@gmail.com>
2021-05-10 01:55:35 +0300

    added login menu entered for different users

--- MobileLibrs/app/src/main/java/com/example/mobilelibrs/Activity_Login.kt
+++ MobileLibrs/app/src/main/java/com/example/mobilelibrs/Activity_Login.kt
@@ -28,69 +28,133 @@ class Activity_Login : AppCompatActivity() {
         var tvresend_verification_email = findViewById<TextView>(R.id.tvresend_verification_email)
         var flag = 0
 
-        if(etEmail.text.toString().equals("mlrs.library2021@gmail.com"))
-        {
-            flag=1
-        }
-        if(etEmail.text.toString().equals("ybeyza.ankara@gmail.com"))
-        {
-            flag=2
-        }
-        if(etEmail.text.toString().equals("yaratici2007@gmail.com"))
-        {
-            flag=3
-        }
 
 
         // login btn click listener
         btnEmail_sign_in_button.setOnClickListener {
 
+            // this email for login admin menu page
+            if(etEmail.text.toString().equals("mlrs.library2021@gmail.com"))
+                flag=1
+            // this email for login lm menu page
+            if(etEmail.text.toString().equals("ybeyza.ankara@gmail.com"))
+                flag=2
+            // this email for librarian admin menu page
+            if(etEmail.text.toString().equals("yaratici2007@gmail.com"))
+                flag=3
             if (etEmail.text.isNotEmpty() && etPassword.text.isNotEmpty()) {
                 showProgressbar()
-
-                FirebaseAuth.getInstance().signInWithEmailAndPassword(etEmail.text.toString(), etPassword.text.toString())
+                if(flag == 1) {
+                    FirebaseAuth.getInstance().signInWithEmailAndPassword(
+                        etEmail.text.toString(),
+                        etPassword.text.toString()
+                    )
                         .addOnCompleteListener(object : OnCompleteListener<AuthResult> {
                             override fun onComplete(p0: Task<AuthResult>) {
-                                    // if user is admin
-                                    if(flag == 1) {
-                                        reDirect_Admin_MenuPage()
-                                        closeProgressBar()
-                                        Toast.makeText(this@Activity_Login, "Admin Login Successful: " + p0.isSuccessful, Toast.LENGTH_SHORT).show()
-                                    }
-                                    // if user is lib mem
-                                    else if(flag == 2)
-                                    {
-                                        reDirect_LM_MenuPage()
-                                        closeProgressBar()
-                                        Toast.makeText(this@Activity_Login, "Library Member Login Successful: " + p0.isSuccessful, Toast.LENGTH_SHORT).show()
-
-                                    }
-                                    // if user is librarian
-                                    else if(flag == 3)
-                                    {
-                                        reDirect_Librarian_MenuPage()
-                                        closeProgressBar()
-                                        Toast.makeText(this@Activity_Login, "Librarian Login Successful: " + p0.isSuccessful, Toast.LENGTH_SHORT).show()
-
-                                    }
-                                }
+                                // if user is admin
+                                reDirect_Admin_MenuPage()
+                                closeProgressBar()
+                                Toast.makeText(
+                                    this@Activity_Login,
+                                    "Admin Login Successful: " + p0.isSuccessful,
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
 
                         })
                         .addOnFailureListener(object : OnFailureListener {
                             override fun onFailure(p0: Exception) {
-                                Toast.makeText(this@Activity_Login, "Login Failed: " + p0.message, Toast.LENGTH_SHORT).show()
+                                Toast.makeText(
+                                    this@Activity_Login,
+                                    "Login Failed: " + p0.message,
+                                    Toast.LENGTH_SHORT
+                                ).show()
                                 closeProgressBar()
                             }
-
                         })
+                    }
+                    if (flag == 2) {
+                        FirebaseAuth.getInstance().signInWithEmailAndPassword(
+                            etEmail.text.toString(),
+                            etPassword.text.toString()
+                        )
+                            .addOnCompleteListener(object : OnCompleteListener<AuthResult> {
+                                override fun onComplete(p0: Task<AuthResult>) {
+                                    // if user is LM
+                                    reDirect_LM_MenuPage()
+                                    closeProgressBar()
+                                    Toast.makeText(
+                                        this@Activity_Login,
+                                        "Library Member Login Successful: " + p0.isSuccessful,
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                }
 
+                            })
+                            .addOnFailureListener(object : OnFailureListener {
+                                override fun onFailure(p0: Exception) {
+                                    Toast.makeText(
+                                        this@Activity_Login,
+                                        "Login Failed: " + p0.message,
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                    closeProgressBar()
+                                }
+                            })
+                    }
+
+                    if (flag == 3) {
+                        FirebaseAuth.getInstance().signInWithEmailAndPassword(
+                            etEmail.text.toString(),
+                            etPassword.text.toString()
+                        )
+                            .addOnCompleteListener(object : OnCompleteListener<AuthResult> {
+                                override fun onComplete(p0: Task<AuthResult>) {
+                                    // if user is librarian
+                                    reDirect_Librarian_MenuPage()
+                                    closeProgressBar()
+                                    Toast.makeText(
+                                        this@Activity_Login,
+                                        "Librarian Login Successful: " + p0.isSuccessful,
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                }
+
+                            })
+                            .addOnFailureListener(object : OnFailureListener {
+                                override fun onFailure(p0: Exception) {
+                                    Toast.makeText(
+                                        this@Activity_Login,
+                                        "Login Failed: " + p0.message,
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                    closeProgressBar()
+                                }
+                            })
+                    }
 
             } else {
                 Toast.makeText(this, "Please fill in the blank fields.", Toast.LENGTH_LONG).show()
             }
         }
+/*
+        // if user is lib mem
+        else if(flag == 2)
+        {
+            reDirect_LM_MenuPage()
+            closeProgressBar()
+            Toast.makeText(this@Activity_Login, "Library Member Login Successful: " + p0.isSuccessful, Toast.LENGTH_SHORT).show()
+        }
 
+        // if user is librarian
+        else if(flag == 3)
+        {
+            reDirect_Librarian_MenuPage()
+            closeProgressBar()
+            Toast.makeText(this@Activity_Login, "Librarian Login Successful: " + p0.isSuccessful, Toast.LENGTH_SHORT).show()
 
+        }
+*/
         tvlink_register.setOnClickListener {
             var intent = Intent(this, Activity_Register::class.java)
             startActivity(intent)
