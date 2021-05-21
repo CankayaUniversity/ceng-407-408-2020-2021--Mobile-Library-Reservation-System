5793fec85930b5e890a81007b5f5d56131db28c1
DESKTOP-8R5D2NM\BEYZA <yaratici2007@gmail.com>
2021-05-21 17:31:19 +0300

    qr code page redirect main menu

--- MobileLibrs/app/src/main/java/com/example/mobilelibrs/Activity_LM_QR.kt
+++ MobileLibrs/app/src/main/java/com/example/mobilelibrs/Activity_LM_QR.kt
@@ -1,12 +1,30 @@
 package com.example.mobilelibrs
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.mobilelibrs.databinding.LayoutLmListLibrariesBinding
+import com.example.mobilelibrs.databinding.LayoutLmQrCodeBinding
 
 class Activity_LM_QR : AppCompatActivity() {
 
+    lateinit var binding1: LayoutLmQrCodeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.layout_lm_qr_code)
+
+        val binding1 = LayoutLmQrCodeBinding.inflate(layoutInflater)
+        // click make reservation button, view list libraries.
+        setContentView(binding1.root)
+
+        // Reservation finished in page. Redirect main menu.
+        binding1.btnBackMenu.setOnClickListener(){
+            reDirectMainMenu()
+        }
+    }
+
+    fun reDirectMainMenu() {
+        val intent = Intent(this, Activity_LM_Menu::class.java)
+        startActivity(intent)
+        finish()
     }
 }
