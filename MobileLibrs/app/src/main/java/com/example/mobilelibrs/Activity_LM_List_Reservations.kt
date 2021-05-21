df4b11a3f28b48b206674da267fca3cee0d091ac
DESKTOP-8R5D2NM\BEYZA <yaratici2007@gmail.com>
2021-05-21 19:51:51 +0300

    list reservation connect other page

--- MobileLibrs/app/src/main/java/com/example/mobilelibrs/Activity_LM_List_Reservations.kt
+++ MobileLibrs/app/src/main/java/com/example/mobilelibrs/Activity_LM_List_Reservations.kt
@@ -1,13 +1,30 @@
 package com.example.mobilelibrs
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.mobilelibrs.databinding.LayoutLmListReservationsBinding
+import com.example.mobilelibrs.databinding.LayoutLmQrCodeBinding
 
 class Activity_LM_List_Reservations : AppCompatActivity() {
 
+    lateinit var binding: LayoutLmListReservationsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // click make reservation button, view list libraries.
-        setContentView(R.layout.layout_lm_list_reservations)
+
+
+        val binding1 = LayoutLmListReservationsBinding.inflate(layoutInflater)
+        setContentView(binding1.root)
+        // click make reservation button, view menu of lm.
+        binding1.btnBack.setOnClickListener(){
+            reDirectMainMenu()
+        }
+    }
+
+    // Reservation finished in page. Redirect main menu.
+    fun reDirectMainMenu() {
+        val intent = Intent(this, Activity_LM_Menu::class.java)
+        startActivity(intent)
+        finish()
     }
 }
