dd43049d0c170e89b49d99f8f9c5d36ec1799512
burcubiterr <70144117+burcubiterr@users.noreply.github.com>
2021-05-24 00:03:03 +0300

    Activity_LM_List_Reservations veri çekildi.

--- MobileLibrs/app/src/main/java/com/example/mobilelibrs/Activity_LM_List_Reservations.kt
+++ MobileLibrs/app/src/main/java/com/example/mobilelibrs/Activity_LM_List_Reservations.kt
@@ -2,29 +2,68 @@ package com.example.mobilelibrs
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import java.lang.StringBuilder
+import com.example.mobilelibrs.Library
+import com.example.mobilelibrs.databinding.LayoutLmChooseTableBinding
+import com.example.mobilelibrs.databinding.LayoutLmListLibrariesBinding
 import com.example.mobilelibrs.databinding.LayoutLmListReservationsBinding
-import com.example.mobilelibrs.databinding.LayoutLmQrCodeBinding
 
-class Activity_LM_List_Reservations : AppCompatActivity() {
+class Activity_LM_List_Reservations: AppCompatActivity() {
 
     lateinit var binding: LayoutLmListReservationsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val binding = LayoutLmListReservationsBinding.inflate(layoutInflater)
+        binding.txtAreaReservations.movementMethod = ScrollingMovementMethod()
 
-        val binding1 = LayoutLmListReservationsBinding.inflate(layoutInflater)
-        setContentView(binding1.root)
-        // click make reservation button, view menu of lm.
-        binding1.btnBack.setOnClickListener(){
-            reDirectMainMenu()
+        // get db reference
+        var database = FirebaseDatabase.getInstance().reference.child("reservation")
+        setContentView(binding.root)
+
+
+
+
+        // All data is in getData variable
+        var getData = object : ValueEventListener {
+            //bu method veritabanında veri varsa anında görüntülüyor
+            override fun onDataChange(snapshot: DataSnapshot) {
+                var sb = StringBuilder() // convert all data as text
+                for(i in snapshot.children) {
+                    // path: database de tutulan değişkenin ismi
+                    var libName = i.child("libName").getValue().toString()
+                    sb.append("Library: $libName \n")
+                    var fromtime = i.child("fromtime").getValue().toString()
+                    sb.append("Time: $fromtime - ")
+                    var totime= i.child("totime").getValue().toString()
+                    sb.append("$totime \n")
+                    var date = i.child("date").getValue().toString()
+                    sb.append("Date: $date  ")
+                    var tableno = i.child("tableNo").getValue().toString()
+                    sb.append("Table: $tableno\n\n")
+                }
+                binding.txtAreaReservations.setText(sb)
+
+            }
+            override fun onCancelled(error: DatabaseError) {
+
+            }
         }
+        database.addValueEventListener(getData)
+        database.addListenerForSingleValueEvent(getData)
     }
 
-    // Reservation finished in page. Redirect main menu.
-    fun reDirectMainMenu() {
-        val intent = Intent(this, Activity_LM_Menu::class.java)
-        startActivity(intent)
-        finish()
-    }
+
 }
