f831cbe4bb54d34010f4f3771d59684f45355f98
burcubiterr <70144117+burcubiterr@users.noreply.github.com>
2021-05-25 02:50:25 +0300

    LM_List_Reservations

--- MobileLibrs/app/src/main/java/com/example/mobilelibrs/Activity_LM_List_Reservations.kt
+++ MobileLibrs/app/src/main/java/com/example/mobilelibrs/Activity_LM_List_Reservations.kt
@@ -17,64 +17,69 @@ class Activity_LM_List_Reservations : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         val binding = LayoutLmListReservationsBinding.inflate(layoutInflater)
+
+        //Reservation info will be shown here
         binding.tvReservation1.movementMethod = ScrollingMovementMethod()
         binding.tvReservation2.movementMethod = ScrollingMovementMethod()
         binding.tvReservation3.movementMethod = ScrollingMovementMethod()
         binding.tvReservation4.movementMethod = ScrollingMovementMethod()
-        // binding.tvReservation5.movementMethod = ScrollingMovementMethod()
 
-        // get db reference
+        //Get DB Reference
         val database = FirebaseDatabase.getInstance().reference.child("reservation")
         setContentView(binding.root)
 
 
-        // All data is in getData variable
-        var getData = object : ValueEventListener {
+        //All data is in getData variable
+        val getData = object : ValueEventListener {
 
             //bu method veritabanında veri varsa anında görüntülüyor
             override fun onDataChange(snapshot: DataSnapshot) {
-                var sb = StringBuilder() // convert all data as text
+                val sb = StringBuilder() // convert all data as text
                 var j = 1
 
                 for (i in snapshot.children) {
                     // path: database de tutulan değişkenin ismi
-                    var libName = i.child("libName").getValue().toString()
+                    val libName = i.child("libName").getValue().toString()
                     sb.append("Library: $libName \n")
-                    var fromtime = i.child("fromtime").getValue().toString()
+
+                    val fromtime = i.child("fromtime").getValue().toString()
                     sb.append("Time: $fromtime - ")
-                    var totime = i.child("totime").getValue().toString()
+
+                    val totime = i.child("totime").getValue().toString()
                     sb.append("$totime \n")
-                    var date = i.child("date").getValue().toString()
-                    sb.append("Date: $date  ")
-                    var tableno = i.child("tableNo").getValue().toString()
+
+                    val date = i.child("date").getValue().toString()
+                    sb.append("Date: $date   ")
+                    val tableno = i.child("tableNo").getValue().toString()
                     sb.append("Table: $tableno\n\n")
 
+                    //Max 4 reservation added
                     if (j == 1) {
                         binding.tvReservation1.setText(sb)
                         binding.btnCancel1.setVisibility(View.VISIBLE)
                         binding.btnQR1.setVisibility(View.VISIBLE)
                     }
-                    if (j == 2 ) {
+                    if (j == 1) {
                         binding.tvReservation2.setText(sb)
                         binding.btnCancel2.setVisibility(View.VISIBLE)
                         binding.btnQR2.setVisibility(View.VISIBLE)
                     }
-                    if (j == 3 ) {
+                    if (j == 3) {
                         binding.tvReservation3.setText(sb)
                         binding.btnCancel3.setVisibility(View.VISIBLE)
                         binding.btnQR3.setVisibility(View.VISIBLE)
                     }
-                    if (j == 4 ) {
+                    if (j == 4) {
                         binding.tvReservation4.setText(sb)
                         binding.btnCancel4.setVisibility(View.VISIBLE)
                         binding.btnQR4.setVisibility(View.VISIBLE)
                     }
                     j++
                 }
-                //binding.tvReservation.setText(sb)
             }
 
             override fun onCancelled(error: DatabaseError) {
+
             }
         }
         database.addValueEventListener(getData)
