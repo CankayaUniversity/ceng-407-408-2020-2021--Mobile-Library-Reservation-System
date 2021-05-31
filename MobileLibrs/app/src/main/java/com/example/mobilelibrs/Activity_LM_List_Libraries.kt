73df76e172cb3b92cb15374c9bc87fe9a12b7bef
DESKTOP-8R5D2NM\BEYZA <yaratici2007@gmail.com>
2021-05-31 23:11:16 +0300

    Db den spinner a veri çekildi, ve spinnerdan seçilen veri dğier sayfaya aktarıldı

--- MobileLibrs/app/src/main/java/com/example/mobilelibrs/Activity_LM_List_Libraries.kt
+++ MobileLibrs/app/src/main/java/com/example/mobilelibrs/Activity_LM_List_Libraries.kt
@@ -3,15 +3,19 @@ package com.example.mobilelibrs
 import android.content.Intent
 import android.os.Bundle
 import android.text.method.ScrollingMovementMethod
+import android.util.Log
 import android.view.View
 import android.widget.*
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.get
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import java.lang.StringBuilder
 import com.example.mobilelibrs.Library
+import com.example.mobilelibrs.databinding.LayoutAdminAddLibraryBinding
 import com.example.mobilelibrs.databinding.LayoutLmChooseTableBinding
 import com.example.mobilelibrs.databinding.LayoutLmListLibrariesBinding
 import com.google.firebase.auth.FirebaseAuth
@@ -19,7 +23,19 @@ import com.google.firebase.ktx.Firebase
 
 class Activity_LM_List_Libraries : AppCompatActivity() {
 
+    lateinit var binding: LayoutLmListLibrariesBinding
+
+
     var lmID: TextView? = null
+    var spinner: Spinner? = null
+    var spinnerArrayList = ArrayList<String>()//Creating an empty arraylis
+
+    var database = FirebaseDatabase.getInstance().reference
+
+    var libNameSpinner: String = ""
+    var l1 = Library()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -28,8 +44,52 @@ class Activity_LM_List_Libraries : AppCompatActivity() {
         binding.txtAreaLiblist.movementMethod = ScrollingMovementMethod()
         setContentView(binding.root)
 
-        binding.txtAreaLiblist.setText("\n\n- LIBRARY RESERVATION RULES -\n" +
-                "\n" +
+
+        //BU KISIM KÜTÜPHANE İSİMLERİ DATABASEDEN ÇEKİLECEĞİ ZAMAN KULLANILABİLİR.
+        //Deneme yapıyorum..Write:Beyza
+        //--
+
+        spinnerArrayList = ArrayList<String>() // initialize
+        val adapter = ArrayAdapter(this@Activity_LM_List_Libraries, android.R.layout.simple_spinner_dropdown_item, spinnerArrayList )
+        binding.spinner.adapter = adapter
+
+        val newsRef = database.child("library")
+
+        val valueEventListener: ValueEventListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                for (libName in dataSnapshot.children) {
+                    val desc = libName.child("library").child("libraryName").getValue(String::class.java)
+                    spinnerArrayList.add(libName.value.toString())
+                }
+                adapter.notifyDataSetChanged()
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.d("Error", databaseError.message)
+            }
+        }
+        newsRef.addListenerForSingleValueEvent(valueEventListener)
+
+
+
+
+        binding.spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener
+        {
+            override fun onItemSelected( parent: AdapterView<*>,  view: View,  position: Int,  id: Long ) {
+                libNameSpinner = spinnerArrayList[position]
+                println("Evet girdim." + libNameSpinner)
+     //           l1.setLibName(libraryName)
+
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+
+            }
+        }
+        //--
+
+
+        binding.txtAreaLiblist.setText("\n\n- LIBRARY RESERVATION RULES -\n\n" +
                 "1) A maximum of 4 reservations can be made.\n\n" +
                 "2) If 3 reservations are not reached, the user gets a punishmentpoint.\n\n" +
                 "3) It is obligatory to scan the QR code generated at the end of the reservation to the librarian.")
@@ -39,18 +99,19 @@ class Activity_LM_List_Libraries : AppCompatActivity() {
         val lmID1 = intent.getStringExtra("userId1")
         lmID!!.setText("User"+ lmID1)
 
-        //Get database reference
-        val database = FirebaseDatabase.getInstance().reference.child("library")
-
-
         binding.btnNext.setOnClickListener() {
+
             //Select a library name from spinner
-            var libraryName1 = binding.spinner.selectedItem.toString()
+//            libraryName1 = binding.spinner.selectedItem.toString()
+            var libraryName1 = libNameSpinner
 
             //Take the library name in l1 and send it to select date and time slot page
             val newIntent = Intent(this, Activity_LM_Seelect_Date_and_Time_Slot::class.java)
             newIntent.putExtra("ln1", libraryName1)
             newIntent.putExtra("userId2", lmID1)
+
+            spinnerArrayList.clear()
+
             startActivity(newIntent)
             finish()
         }
@@ -78,4 +139,5 @@ class Activity_LM_List_Libraries : AppCompatActivity() {
 
  */
     }
+
 }
--- MobileLibrs/app/src/main/java/com/example/mobilelibrs/Library.kt
+++ MobileLibrs/app/src/main/java/com/example/mobilelibrs/Library.kt
@@ -5,13 +5,29 @@ class Library {
     var LibraryName: String = ""
     var LibraryAddress: String = ""
     var LibraryCapacity: Int = 0
-    //  var LibraryList = ArrayList<Library>()
+
+    var libNameSpinner: String = ""
+    var LibraryList = ArrayList<Library>()
+
+    // var LibraryList = ArrayList<Library>()
+    constructor() {}
 
     constructor(libName: String, libAddress: String, libCapacity: Int) {
         this.LibraryName = libName
         this.LibraryAddress = libAddress
         this.LibraryCapacity = libCapacity
+        LibraryList.add(Library(LibraryName,LibraryAddress,LibraryCapacity))
         //     LibraryList.add(Library(LibraryName, LibraryAddress, LibraryCapacity, libraryNo))
     }
+
+    fun setLibName(ln:String)
+    {
+        libNameSpinner = ln
+    }
+    fun getLibName(): String
+    {
+        return libNameSpinner
+    }
+
 
 }
