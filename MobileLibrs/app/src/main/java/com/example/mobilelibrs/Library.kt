b68f6732c310b1e7c4285829f4245e6e3c85d1f9
DESKTOP-8R5D2NM\BEYZA <yaratici2007@gmail.com>
2021-06-04 21:24:09 +0300

    exit criteria updates

--- MobileLibrs/app/src/main/java/com/example/mobilelibrs/Library.kt
+++ MobileLibrs/app/src/main/java/com/example/mobilelibrs/Library.kt
@@ -1,9 +1,53 @@
+/*
+public class dbDrakorAdapter {
+
+private String LinkMovie;
+private String List;
+private String FotoPemain;
+private String NamaPemain;
+
+public dbDrakorAdapter (String linkMovie, String list, String fotoPemain, String namaPemain) {
+    LinkMovie = linkMovie;
+    List = list;
+    FotoPemain = fotoPemain;
+    NamaPemain = namaPemain;
+}
+
+public class EpisodeDetails {
+   // public Long chapterId;
+    public String LinkMovie;
+    public String List;
+}
+public class Episode {
+    List<EpisodeDetails> episodeDetails;
+}
+
+public class PemeranDetails {
+    // public Long chapterId;
+    public String LinkMovie;
+    public String List;
+}
+public class Pemeran {
+    List<PemeranDetails> pemeranDetails;
+}
+
+
+public Map<String, Object> toMap() {
+    HashMap<String, Object> result = new HashMap<>();
+    result.put("List", List);
+    result.put("LinkMovie", LinkMovie);
+    result.put("FotoPemain", FotoPemain);
+    result.put("NamaPemain", NamaPemain);
+    return result;
+}
+}*/
 package com.example.mobilelibrs
 
 import java.text.SimpleDateFormat
 import java.time.LocalDate
 import java.util.*
 import java.util.Calendar.YEAR
+
 
 class Library {
 
@@ -17,14 +61,42 @@ class Library {
     var DateList = ArrayList<String>()
     // var LibraryList = ArrayList<Library>()
 
+    var days: String? = null
+    var tables: String? = null
+
+    public class dateDetails {
+        // public Long chapterId;
+        var day: String = ""
+    }
+    public class Datess {
+        var dateDetailss = ArrayList<dateDetails>()
+    }
+    public class TableDetails {
+        // public Long chapterId;
+        var table: String = ""
+    }
+    public class Tables {
+        var dateDetailss = ArrayList<TableDetails>()
+    }
+
+    fun main(){
+
+        var result: HashMap<String, Datess> = HashMap<String, Datess>() //define empty hashmap
+        result.put()
+        result.put()
+    }
+
     constructor(libName: String, libAddress: String, libCapacity: Int) {
         this.LibraryName = libName
         this.LibraryAddress = libAddress
         this.LibraryCapacity = libCapacity
+        main()
+    /*
 
         var dates = arrayOf("09.08.2021", "09.09.2021")
         var tslots = arrayOf("9:00-10:00", "10:00-11:00")
         var tbls = arrayOf("T1", "T2", "T3")
+
 
         for(date in dates) {
             DateList.add(date)
@@ -35,11 +107,11 @@ class Library {
         for(tb in tbls) {
             TableList.add(tb)
         }
-
+*/
         //libraryList.add(LibraryName)
         //     LibraryList.add(Library(LibraryName, LibraryAddress, LibraryCapacity, libraryNo))
     }
-
-
-
 }
+
+
+
