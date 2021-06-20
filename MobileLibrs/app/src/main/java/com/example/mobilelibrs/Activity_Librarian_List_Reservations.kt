923aaaa11b7667ca4ddfc0d670c44153b3e2a460
DESKTOP-8R5D2NM\BEYZA <yaratici2007@gmail.com>
2021-06-20 21:09:25 +0300

    Update Activity_Librarian_List_Reservations.kt

--- MobileLibrs/app/src/main/java/com/example/mobilelibrs/Activity_Librarian_List_Reservations.kt
+++ MobileLibrs/app/src/main/java/com/example/mobilelibrs/Activity_Librarian_List_Reservations.kt
@@ -5,7 +5,10 @@ import android.text.method.ScrollingMovementMethod
 import androidx.appcompat.app.AppCompatActivity
 import com.example.mobilelibrs.databinding.LayoutAdminListLibrariesBinding
 import com.example.mobilelibrs.databinding.LayoutLibrarianListReservationsBinding
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class Activity_Librarian_List_Reservations : AppCompatActivity() {
 
@@ -21,5 +24,27 @@ class Activity_Librarian_List_Reservations : AppCompatActivity() {
         var database = FirebaseDatabase.getInstance().reference.child("reservation")
 
         binding.LibrarianResList.movementMethod = ScrollingMovementMethod()
+
+        //All data is in getData variable
+        var getData = object : ValueEventListener {
+            //All data is shown if they exist
+            override fun onDataChange(snapshot: DataSnapshot) {
+                var sb = StringBuilder() //Metin birleştirme sınıfı
+                for (i in snapshot.children) {
+                    //Path: Datebasede tutulan değişkenin ismi
+                    var lname = i.child("libraryName").getValue()
+                    var laddress = i.child("libraryAddress").getValue()
+                    var lcap = i.child("libraryCapacity").getValue()
+                    sb.append(" Name: $lname \n Address: $laddress \n Capacity: $lcap \n\n")
+                }
+                binding.LibrarianResList.setText(sb)
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        }
+        database.addValueEventListener(getData)
+        database.addListenerForSingleValueEvent(getData)
     }
 }
